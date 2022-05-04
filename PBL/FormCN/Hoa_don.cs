924b439c558f1995e971d9f8626b232336267de7
phucdut <85094642+phucdut@users.noreply.github.com>
2022-05-04 15:45:21 +0700

    Add files via upload

---
+++ PBL/FormCN/Hoa_don.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL.FormCN
+{
+    public partial class Hoa_don : Form
+    {
+        public Hoa_don()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
